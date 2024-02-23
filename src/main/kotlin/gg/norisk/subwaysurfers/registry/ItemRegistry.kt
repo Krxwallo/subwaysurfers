a94f01c6ecd3c0762f381e8f543fd074de26ff88
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2024-02-23 19:32:57 +0100

    feature(client): Added SubwaySurfer ItemGroup

--- src/main/kotlin/gg/norisk/subwaysurfers/registry/ItemRegistry.kt
+++ src/main/kotlin/gg/norisk/subwaysurfers/registry/ItemRegistry.kt
@@ -1,16 +1,38 @@
 package gg.norisk.subwaysurfers.registry
 
 import gg.norisk.subwaysurfers.SubwaySurfers.toId
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
+import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.text.Text
 
 object ItemRegistry {
     val COIN: Item = registerItem("coin", Item(Item.Settings()))
 
-    fun init() {
+    val SUBWAY_RAIL: BlockItem = registerItem("subway_rail", BlockItem(BlockRegistry.SUBWAY_RAIL, Item.Settings()))
+    val TOP_BARRIER: BlockItem = registerItem("top_barrier", BlockItem(BlockRegistry.TOP_BARRIER, Item.Settings()))
+    val BOTTOM_BARRIER: BlockItem =
+        registerItem("bottom_barrier", BlockItem(BlockRegistry.BOTTOM_BARRIER, Item.Settings()))
 
+
+    fun init() {
     }
+
+    val ITEM_GROUP: ItemGroup = Registry.register(
+        Registries.ITEM_GROUP, "subwaysurfers_utils".toId(), FabricItemGroup
+            .builder()
+            .displayName(Text.translatable("itemGroup.subwaysurfers.subwaysurfers_utils"))
+            .icon { ItemStack(SUBWAY_RAIL) }
+            .entries { enabledFeatures: ItemGroup.DisplayContext?, entries: ItemGroup.Entries ->
+                entries.add(SUBWAY_RAIL)
+                entries.add(TOP_BARRIER)
+                entries.add(BOTTOM_BARRIER)
+            }.build()
+    )
 
     private fun <I : Item> registerItem(name: String, item: I): I {
         return Registry.register(Registries.ITEM, name.toId(), item)
